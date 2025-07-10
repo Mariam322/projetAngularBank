ef177cf78c6db7f4de4cc2577631fe33d2b46a1d
Mseddi Mariam <77213539+Mariam322@users.noreply.github.com>
2025-07-10 21:52:44 +0100

    Update GoogleDriveConfig.java

--- Documents/src/main/java/tn/iit/service/GoogleDriveConfig.java
+++ Documents/src/main/java/tn/iit/service/GoogleDriveConfig.java
@@ -1,1 +1,34 @@
+package tn.iit.service;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+@Configuration
+public class GoogleDriveConfig {
+
+ 
+    @Bean
+    public Drive drive() throws Exception {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ClassPathResource("credentials.json").getInputStream())
+                .createScoped(Collections.singleton(DriveScopes.DRIVE_FILE));
+
+        return new Drive.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(),
+                new HttpCredentialsAdapter(credentials))
+                .setApplicationName("Facturation Service")
+                .build();
+    }
+}
